2498f30ba7b91d06de3e53656e154f878ac77b53
Todd Gauthier <toddinator2@yahoo.com>
2024-05-20 14:02:43 -0700

    dynamic import AgoraRTC

--- src/components/private/subscribers/sphere/SubSphereContent.js
+++ src/components/private/subscribers/sphere/SubSphereContent.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './SubSphereContent.css';
 import { MenuContext } from '@/utils/context/global/MenuContext';
 //Physician Search
@@ -7,13 +7,21 @@ import PhysicianSearch from './content/physiciansearch/PhysicianSearch';
 import OfficeComponent from './content/physiciansearch/OfficeComponent';
 import OfficeDetails from './content/physiciansearch/OfficeDetails';
 //Telemed
-import AgoraRTC, { AgoraRTCProvider, useRTCClient } from 'agora-rtc-react';
+import { AgoraRTCProvider } from 'agora-rtc-react';
 //Messages
 import Messages from './content/messaging/Messages';
 
 export default function SubSphereContent() {
+	let client;
 	const [menu] = useContext(MenuContext);
-	const client = useRTCClient(AgoraRTC.createClient({ codec: 'vp9', mode: 'rtc' }));
+
+	useEffect(() => {
+		const initSdk = async () => {
+			const AgoraRTC = (await import('agora-rtc-react')).default;
+			client = AgoraRTC.createClient({ codec: 'vp9', mode: 'rtc' });
+		};
+		initSdk();
+	}, []);
 
 	return (
 		<div className='row d-flex justify-content-center'>
