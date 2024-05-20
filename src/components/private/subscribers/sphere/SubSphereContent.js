7f087a9445f7b5b1e507159319d02f2a2d9e1bac
Todd Gauthier <toddinator2@yahoo.com>
2024-05-20 13:06:58 -0700

    took out all video functionality 2

--- src/components/private/subscribers/sphere/SubSphereContent.js
+++ src/components/private/subscribers/sphere/SubSphereContent.js
@@ -6,25 +6,14 @@ import { MenuContext } from '@/utils/context/global/MenuContext';
 import PhysicianSearch from './content/physiciansearch/PhysicianSearch';
 import OfficeComponent from './content/physiciansearch/OfficeComponent';
 import OfficeDetails from './content/physiciansearch/OfficeDetails';
-//Telemed
-/*
-import AgoraRTC, { AgoraRTCProvider, useRTCClient } from 'agora-rtc-react';
-*/
-import JoinCall from './content/virtual/JoinCall';
-import Videos from './content/virtual/Videos';
 //Messages
 import Messages from './content/messaging/Messages';
 
 export default function SubSphereContent() {
 	const [menu] = useContext(MenuContext);
-	//const client = useRTCClient(AgoraRTC.createClient({ codec: 'vp9', mode: 'rtc' }));
 
 	return (
 		<div className='row d-flex justify-content-center'>
-			{/*<AgoraRTCProvider client={client}>*/}
-			{menu.dets === 'joinCall' && <JoinCall />}
-			{menu.vids && <Videos />}
-			{/*</AgoraRTCProvider>*/}
 			<div className='sphContainer red'>
 				{menu.type === 'phySearch' && <PhysicianSearch />}
 				{menu.type === 'messages' && <Messages />}
--- src/components/private/subscribers/sphere/sidebar/Sidebar.js
+++ src/components/private/subscribers/sphere/sidebar/Sidebar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Sidebar.css';
 import Header from './header/SbHeader';
 import PhyButton from './modules/physicians/phybutton/PhyButton';
-import VirtualButton from './modules/virtual/VirtualButton';
 import MessageButton from './modules/messaging/MessageButton';
 
 export default function Sidebar() {
@@ -10,7 +9,6 @@ export default function Sidebar() {
 		<div className='subSideContainer'>
 			<Header />
 			<PhyButton />
-			<VirtualButton />
 			<MessageButton />
 		</div>
 	);
