02ab95a9ba84730d69cd80750c2e4b5e05abb8a8
Todd Gauthier <toddinator2@yahoo.com>
2024-05-27 16:04:05 -0700

    frontend should be completely done now

--- src/app/physicians/learnmore/page.jsx
+++ src/app/physicians/learnmore/page.jsx
@@ -182,7 +182,7 @@ export default function PhyLearnMore() {
 							</div>
 						</div>
 						<div className='w-full lg:w-1/2 lg:ps-7'>
-							<div className='w-full mx-auto sm:px-7 py-5 flex-auto rounded-3xl border-4 border-drkgry'>
+							<div className='w-full mx-auto px-2 sm:px-7 py-5 flex-auto rounded-3xl border-4 border-drkgry'>
 								<div className='w-full mb-4 text-center font-semibold text-xl'>Special Offer &ndash; Early Adopter</div>
 								<div className='w-full mb-4 text-center font-semibold text-3xl'>Get Licensed for</div>
 								<div className='w-full text-center font-semibold text-7xl'>
